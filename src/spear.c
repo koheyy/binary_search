c2088210533be35a26bbd43812595925367f621f
Kouhei Iwasaki <yo.yo.iwasaki@gmail.com>
2020-07-01 17:59:10 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+  int s = 0;
+  for(int i = 0; i < n; i++){
+    s = s + A[i] / x;
+  }
+  if(s >= k) return 1;
+  return 0;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +19,19 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  int mx = 0;
+  for(i = 0; i < n; i++){
+    if(A[i] >= mx) mx = A[i];
+  }
+  lb = 1;
+  ub = mx;
+  if(p(1) == 0) return printf("%d\n", 0);
+  if(p(mx)) return printf("%d\n", mx);
+  while(ub - lb > 1){
+    int mid = (ub + lb) / 2;
+    if(p(mid)) lb = mid;
+    else ub = mid;
+  }
+  printf("%d\n", lb);
   return 0;
 }
