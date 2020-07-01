8893dd8c25684c8ee593a867ecc9c4d3a71c9dc3
Kouhei Iwasaki <yo.yo.iwasaki@gmail.com>
2020-07-01 17:58:30 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+  int s = 0;
+  for(int i = 0; i < n; i++){
+    s = s + (A[i] + x - 1) / x;
+  }
+  if(s <= k) return 1;
+  return 0;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +19,18 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  int mx = 0;
+  for(i = 0; i < n; i++){
+    if(A[i] > mx) mx = A[i];
+  }
+  lb = 1;
+  ub = mx;
+  if(p(1)) return printf("%d\n", 1);
+  while(ub - lb > 1){
+    int mid = (lb + ub)/2;
+    if(p(mid)) ub = mid;
+    else lb = mid;
+  }
+  printf("%d\n", ub);
   return 0;
 }
