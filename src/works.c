75ce4832f63e67bde0936b0b02e569d85a7ef2ab
Kouhei Iwasaki <yo.yo.iwasaki@gmail.com>
2020-07-01 17:59:44 +0900

    revised

--- src/works.c
+++ src/works.c
@@ -4,6 +4,19 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+  int sum = 0;
+  int num = 1;
+  for(int i = 0; i < n; i++){
+    sum = sum + A[i];
+    if(sum > x){
+      num = num + 1;
+      sum = A[i];
+    }
+  }
+  if(num <= k) return 1;
+  else return 0;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +24,20 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  int max = 0;
+  int sum = 0;
+  for(i = 0; i < n; i++){
+    sum = sum + A[i];
+    if(max < A[i]) max = A[i];
+  }
+  lb = max;
+  ub = sum;
+  if(p(max)) return printf("%d\n", max);
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)) ub = mid;
+    else lb = mid;
+  }
+  printf("%d\n",ub);
   return 0;
 }
